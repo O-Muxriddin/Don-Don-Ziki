eefeffb83b36f58ca78f4fcf58307412636cadd3
Muhriddin <muhriddin@mozej.com>
2025-12-14 01:37:00 +0500

    commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+import { elHands,elUser,elRobot, elRefershGame, elGameZone,elResultZone} from "./html-el.js";
+
+//   robot choose 
+function robotchoose() {
+    const hands = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.trunc(Math.random() * hands.length);
+    return hands[randomIndex];
+};
+// swap zones   
+
+function swapZone (boolean) {
+     if (boolean) {
+        elGameZone.style.display = 'none';
+         elResultZone.style.display = 'flex';
+     }
+        else {
+         elGameZone.style.display = 'flex';
+         elResultZone.style.display = 'none';
+}}
+
+// Find winner
+ function findWinner (user, robot) {
+    if (user === robot) {
+        return 'TIE';
+    }else if (user === 'paper' && robot === 'scissors' || user === 'rock' && robot === 'paper' || user === 'scissors' && robot === 'rock') {
+        return 'ROBOT';
+    }else  if (user === 'scissors' && robot === 'rock' || user === 'rock' && robot === 'paper' || user === 'scissors' && robot === 'rock') {
+        return 'ROBOT';
+    }else  if (user === 'rock' && robot === 'paper' || user === 'rock' && robot === 'paper' || user === 'scissors' && robot === 'rock') {
+        return 'ROBOT';
+    }else {
+        return 'USER';
+    }
+ }
+
+// Hand
+elHands.forEach(elhand => {
+    elhand.addEventListener('click', (evnt) => {
+         swapZone (true);
+            
+        const user = evnt.target.alt;
+        const robot = robotchoose();
+  
+
+        elUser.src = evnt.target.src;
+        elRobot.src = "./img/hand-load.svg";
+
+        setTimeout(() => {
+            elRobot.src = `./img/${robot}.png`;
+          const winner = findWinner(user, robot);
+            alert(winner);
+        }, 1000 ,)
+        ;
+    })});
+
+// Refresh game
+elRefershGame.addEventListener('click', () => {
+    swapZone (false);
+});
+
